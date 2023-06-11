716a952a425a4ee90a28b23ebc53b021fb361b7d
Melih Emre Güler <melihguler@Melihs-MacBook-Pro.local>
2023-06-11 21:57:56 +0300

    add renderTemplate func

---
+++ render.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	// ParseFiles returns a pointer to a template and an error
+	parsedTemplate, _ := template.ParseFiles("templates/" + tmpl)
+
+	// Execute applies a parsed template to the specified data object, writing the output to writer w.
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsing template", err)
+		return
+	}
+
+	fmt.Println(tmpl, "rendered successfully")
+}
