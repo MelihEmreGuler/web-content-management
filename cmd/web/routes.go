419280329917c18a4c0ad1f2c13134a91999b921
Melih Emre Güler <melihguler@Melihs-MacBook-Pro.local>
2023-07-01 15:49:26 +0300

    add routes function

---
+++ cmd/web/routes.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/MelihEmreGuler/web-content-management/pkg/handlers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
+)
+
+func routes() http.Handler {
+	mux := chi.NewRouter()
+
+	// middlewares are executed in the order they are defined
+	mux.Use(middleware.Recoverer) // recovering from panics
+	mux.Use(WriteToConsole)       // write to console
+	mux.Use(NoSurf)               // no csrf attacks
+	mux.Use(SessionLoad)          // load and save session on every request
+
+	mux.Get("/home", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+	return mux
+
+	/*mux := pat.New()
+	mux.Get("/home", http.HandlerFunc(handlers.Repo.Home))
+	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	return mux*/
+}
